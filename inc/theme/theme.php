a12080a567725a7db630c93dca15ed4188d763eb
Tim <timnijborg@hotmail.nl>
2014-04-19 15:45:16 +0200

    Menu in theme.php gezet

--- inc/theme/theme.php
+++ inc/theme/theme.php
@@ -37,8 +37,34 @@
 	}
 
     echo $balk2; 
-    echo $menulinks; 
-	
+
+    ?>
+
+    <div class="sidebar-nav">
+        <br/>
+
+        <a href="#dashboard-menu" class="nav-header" data-toggle="collapse"><i class="icon-dashboard"></i>Dashboard<i class="icon-chevron-up"></i></a>
+        <ul id="dashboard-menu" class="nav nav-list collapse in">
+            <li><a href="<?= SEO_LINK . HOME; ?>">Home</a></li>
+            <li ><a href="<?=  SEO_LINK . NEWS; ?>">Nieuws</a></li>
+        </ul>
+
+        <a href="#products-menu" class="nav-header" data-toggle="collapse"><i class="icon-briefcase"></i>Producten<i class="icon-chevron-up"></i></a>
+        <ul id="products-menu" class="nav nav-list collapse in">
+            <li ><a href="<?=  SEO_LINK . PRODUCTS; ?>">Producten <span class="label label-info"><?=  KLANT_PRODUCTEN_TOTAAL_MENU; ?></a></span></li>
+            <li ><a href="<?=  SEO_LINK . NIEUWE_OFFERTE; ?>">Nieuw product/offerte</a></li>
+            <li ><a href="<?=  SEO_LINK . FACTUREN; ?>">Facturen <span class="label label-info"><?=  KLANT_FACTUUR_TOTAAL_MENU; ?></a></span></li>
+        </ul>
+
+        <a href="#support-menu" class="nav-header" data-toggle="collapse"><i class="icon-question-sign"></i>Support<i class="icon-chevron-up"></i></a>
+        <ul id="support-menu" class="nav nav-list collapse in">
+            <li ><a href="#">Veel gestelde vragen</a></li>
+            <li ><a href="#">Forum</a></li>
+            <li ><a href="<?= SEO_LINK . SUPPORT_TICKETS; ?>">Support tickets</a></li>
+            <li ><a href="<?= SEO_LINK . SUPPORT_TICKETS_NEW; ?>">Nieuwe support ticket</a></li>
+        </ul>
+
+	<?php
 	if(TOEGANG_LOC == 0) {
 	if(TOEGANG > "0"){ 
 	?>
