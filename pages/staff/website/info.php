b11cb300a2e21e1a685b2280c235880ac83560f3
Tim <timnijborg@hotmail.nl>
2014-04-19 13:39:43 +0200

    Php info toegevoegd

--- pages/staff/website/info.php
+++ pages/staff/website/info.php
@@ -2,13 +2,12 @@
     <ul class="nav nav-tabs">
       <li class="active"><a href="#info" data-toggle="tab">Informatie</a></li>
       <li><a href="#change" data-toggle="tab">Changelog</a></li>
+      <li><a href="#software" data-toggle="tab">PHP info</a></li>
     </ul>
     <div id="myTabContent" class="tab-content">
       <div class="tab-pane active in" id="info">
         Informatie of software, hardware etc.
-		<br/>
-		<strong>____________________________________________________</strong>
-		<br/><br/>
+		<hr size="1">
 		<strong>Developers4you versie =</strong> <? ECHO VERSION; ?><br/>
 		<strong>Ontwikkelaar =</strong> Tim Nijborg -> skype mentimjojo
       </div>
@@ -19,6 +18,13 @@
 		<br/>
 		<strong>Changelog 2:</strong> <a href="http://customers.developers4you.nl/pages/changelogs/changelog2.txt" target="_blank">Klik hier</a>
       </div>
+        <div class="tab-pane active in" id="software">
+            Alles over de php versie:
+            <hr size="1">
+            <?php
+            echo phpinfo();
+            ?>
+        </div>
   </div>
 
 </div>
