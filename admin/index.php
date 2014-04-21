ea2ebf767576a7b9dd504499a163007adeac9a94
Tim <timnijborg@hotmail.nl>
2014-04-21 10:50:12 +0200

    admin begin

--- admin/index.php
+++ admin/index.php
@@ -4,8 +4,10 @@ include_once("inc/config.php");
 
 // Login check
 if($_COOKIE['ADMIN_SYSTEM'] == ""){
+    // Login include
     include_once("login.php");
 } else {
     // Ingelogd
+    include_once("theme/index.php");
 }
 ?>
